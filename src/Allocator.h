9e89a01dffbc41383c73592d4c21145a17c0b6a4
zhanghuanzj <840796340@qq.com>
2016-06-22 22:33:13 +0800

    迭代器
    1.完成内存的分配，释放（通过free-list及内存池来实现）
    2.完成类型萃取信息的编写
    3.完成迭代器信息萃取的编写

---
+++ src/Allocator.h
@@ -0,0 +1,46 @@
+#ifndef _MINI_STL_ALLOCATOR_H_
+#define _MINI_STL_ALLOCATOR_H_ 
+
+#include <Alloc.h>
+
+template<class T>
+class Allocator
+{
+	public:
+		typedef T			value_type;
+		typedef T*			pointer;
+		typedef const T*	const_pointer;
+		typedef T&			reference;
+		typedef const T&	const_reference;
+		typedef size_t		size_type;
+		typedef ptrdiff_t	difference_type;
+	public:
+		static T *allocate();                               //分配一个T大小的内存
+		static T *allocate(size_t n);						//分配n个T大小的内存
+		static void deallocate(T *p);						//回收一个T大小的内存
+		static void deallocate(T *p, size_t n);				//回收n个T大小的内存
+};
+
+template<class T>
+T *allocator<T>::allocate(){
+	return static_cast<T *>(alloc::allocate(sizeof(T)));
+}
+
+template<class T>
+T *allocator<T>::allocate(size_t n){
+	if (n == 0) return 0;
+	return static_cast<T *>(alloc::allocate(sizeof(T) * n));
+}
+
+template<class T>
+void allocator<T>::deallocate(T *p){
+	alloc::deallocate(static_cast<void *>(p), sizeof(T));
+}
+
+template<class T>
+void allocator<T>::deallocate(T *p, size_t n){
+	if (n == 0) return;
+	alloc::deallocate(static_cast<void *>(p), sizeof(T)* n);
+}
+
+#endif
